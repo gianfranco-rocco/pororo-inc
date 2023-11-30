fefa7d10c22f72121f1e3168ae955c38293637ae
Gianfranco Rocco <gianfrancorocco.business@gmail.com>
2023-11-30 16:34:07 -0300

    feat: migrations

---
+++ database/migrations/2023_11_30_192731_create_patient_data_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Domain\Users\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('patient_data', function (Blueprint $table) {
+            $table->id();
+            $table->text('conditions')->nullable();
+            $table->text('weight')->nullable();
+            $table->text('height')->nullable();
+            $table->date('birth_date')->nullable();
+            $table
+                ->foreignIdFor(User::class, 'patient')
+                ->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_data');
+    }
+};
